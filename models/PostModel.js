673c701c489546e2013c76c48d9dbf9496947003
Sid <96074317+node0112@users.noreply.github.com>
2023-05-17 21:30:42 +0100

    added author to post model

--- models/PostModel.js
+++ models/PostModel.js
@@ -5,12 +5,14 @@ const Schema = mongoose.Schema
 const PostSchema = new mongoose.Schema({
     title: {type: String, required: true, minLength: 3, maxLength: 100},
     user: {type: Schema.Types.ObjectId, required: true},
+    author: {type: String, required: true},
     date: {type: Date, required: true},
     likes: {type: Number},
     summary: {type: String, required: true, minLength: 5},
     content: {type: String, required: true, minLength: 3},
     draft: {type: Boolean, required: true},
     backgroundColor : {type: String, required: true},
+    
     textColor: {type: String, required: true},
 })
 
